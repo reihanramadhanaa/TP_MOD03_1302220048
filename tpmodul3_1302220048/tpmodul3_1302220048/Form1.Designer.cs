52ca49deb9738b5a10452b7e85b12c3938d3b353
Ramadhana <ramadhanaa44@gmail.com>
2024-03-04 00:39:34 +0700

    Sudah Selesai

---
+++ tpmodul3_1302220048/tpmodul3_1302220048/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace tpmodul3_1302220048
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.BackColor = System.Drawing.Color.Orange;
+            this.textBoxInput.Location = new System.Drawing.Point(44, 86);
+            this.textBoxInput.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(264, 22);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.BackColor = System.Drawing.Color.Orange;
+            this.buttonSubmit.Location = new System.Drawing.Point(316, 97);
+            this.buttonSubmit.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(133, 37);
+            this.buttonSubmit.TabIndex = 1;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = false;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.BackColor = System.Drawing.Color.Orange;
+            this.labelOutput.Location = new System.Drawing.Point(44, 127);
+            this.labelOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(264, 25);
+            this.labelOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 213);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.textBoxInput);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
---
+++ tpmodul3_1302220048/tpmodul3_1302220048/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace tpmodul3_1302220048
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            string namaPraktikan = textBoxInput.Text;
+
+            if (!string.IsNullOrWhiteSpace(namaPraktikan))
+            {
+                labelOutput.Text = "Halo " + namaPraktikan;
+            }
+            else
+            {
+                MessageBox.Show("Mohon isi nama praktikan terlebih dahulu!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
